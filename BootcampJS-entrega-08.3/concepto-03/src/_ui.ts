d119a70727dbf6c3a7c53253a0f286f7974e7336
Javilone <javilone.blummer@gmail.com>
2024-09-13 11:52:26 +0200

    Refactorizado concepto 03

--- BootcampJS-entrega-08.3/concepto-03/src/_ui.ts
+++ BootcampJS-entrega-08.3/concepto-03/src/_ui.ts
@@ -2,8 +2,11 @@ export const paintMainDiv = () => {
   const mainDiv = document.getElementById("mainDiv");
 
   if (mainDiv && mainDiv instanceof HTMLDivElement) {
-    paintChildDiv(mainDiv);
-    // paintChildDiv(mainDiv);
+    mainDiv.classList.add("mainDiv");
+
+    for (let i = 0; i < 12; i++) {
+      paintChildDiv(mainDiv);
+    }
   }
 };
 
@@ -11,17 +14,17 @@ const paintChildDiv = (mainDiv: HTMLDivElement) => {
   const childDiv = document.createElement("div");
   childDiv.classList.add("childDiv");
   mainDiv?.appendChild(childDiv);
-  childDiv.addEventListener("click", () => childImage(childDiv));
+  // childDiv.addEventListener("click", () => childImage(childDiv));
 };
 
-const childImage = (childDiv: HTMLDivElement) => {
-  if (childDiv.querySelector("img")) {
-    return;
-  }
+// const childImage = (childDiv: HTMLDivElement) => {
+//   if (childDiv.querySelector("img")) {
+//     return;
+//   }
 
-  const image = document.createElement("img");
-  image.classList.add("cardStyle");
-  image.src = "/src/crocodile.svg";
+//   const image = document.createElement("img");
+//   image.classList.add("cardStyle");
+//   image.src = "/src/crocodile.svg";
 
-  childDiv.appendChild(image);
-};
+//   childDiv.appendChild(image);
+// };
